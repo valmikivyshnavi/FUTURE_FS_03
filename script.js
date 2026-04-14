bcb4964d1fce9f21cf7ff4fdff3b4820cf09c206
Vyshnavi <valmikivyshnavi18@gmail.com>
2026-04-15 00:46:15 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function sendMessage() {
-    document.getElementById("success").innerText = "✅ Message sent successfully!";
+function scrollToMenu() {
+    document.getElementById("menu").scrollIntoView({
+        behavior: "smooth"
+    });
 }
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thank you! We will contact you soon.");
+});
